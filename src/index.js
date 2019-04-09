ca51d7d894817ed1ca1a121513e46ebba3fc29e6
Dgiovanelli32 <d.giovanelli32@gmail.com>
2019-04-09 18:49:33 -0300

    Chaging manually

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const app = express();
 //using mlab for free (without docker) 
@@ -12,8 +13,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 //using morgan to catch logs
 app.use(morgan('dev'));
-
-
+//using static express to liberate the images acess
+app.use(
+    "/files", 
+    express.static(path.resolve(__dirname ,"..","tmp", "uploads" ))
+    );
 
 app.use(require('./routes'));
 
